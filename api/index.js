d8d36365eae964beb2857be29f0cee18c38977eb
roy-hcs <chenxingying@hcs55.com>
2025-04-29 13:58:56 +0800

    chore: Cors config

--- api/index.js
+++ api/index.js
@@ -11,7 +11,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Import your routes
